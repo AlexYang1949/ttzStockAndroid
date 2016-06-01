879d107be334b8979083766c97baa338719ead06
alexYang <zhaoheng.yang@zhulebei.com>
2016-06-01 18:59:56 +0800

    修改个推接收

---
+++ ttzAndroid/app/src/main/java/com/ttz/ttzandroid/PushReciver.java
@@ -0,0 +1,13 @@
+package com.ttz.ttzandroid;
+
+import android.content.BroadcastReceiver;
+import android.os.Bundle;
+
+public class PushReciver extends BroadcastReceiver {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
