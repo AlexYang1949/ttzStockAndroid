f0eccb8a6cf3e6805d2241a706043d06c4fce139
alexYang <zhaoheng.yang@zhulebei.com>
2016-06-01 19:02:33 +0800

    修改onCreate

--- ttzAndroid/app/src/main/java/com/ttz/ttzandroid/PushReciver.java
+++ ttzAndroid/app/src/main/java/com/ttz/ttzandroid/PushReciver.java
@@ -1,13 +1,16 @@
 package com.ttz.ttzandroid;
 
 import android.content.BroadcastReceiver;
-import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
 
 public class PushReciver extends BroadcastReceiver {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+        String msg = intent.getStringExtra("message");
+        Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
     }
 }
